519df839434c867681c3e20afa535de364531cb8
John0104Nicholas <154896634+John0104Nicholas@users.noreply.github.com>
2024-01-04 16:22:08 +0530

    flag.js
    please check

--- flag.js
+++ flag.js
@@ -1,21 +1,20 @@
-// const christmasIsland = {
-//     // ... (your Christmas Island JSON data)
-// };
+// XML HTTP Request : It is used to interact with servers via API.
+// Why : To get the data from the Server via API
 
-const restCountriesAPI = 'https://restcountries.com/v3/all';
-
-// Fetch data from Rest Countries API
-fetch(restCountriesAPI)
-    .then(response => response.json())
-    .then(data => {
-        // Extract and print details for all countries
-        data.forEach(country => {
-            const name = country.name.common;
-            const region = country.region;
-            const subregion = country.subregion;
-            const population = country.population;
-
-            console.log(`Name: ${name}, Region: ${region}, Subregion: ${subregion}, Population: ${population}`);
-        });
-    })
-    .catch(error => console.error('Error fetching data from Rest Countries API:', error));
+// Step 01: Create a XHR Object
+// var variableName = userdefined your choice
+var request = new XMLHttpRequest();
+// Step 02: Specify the API
+request.open('GET', 'https://restcountries.com/v2/all');
+// Step 03: Send the request to the server
+request.send();
+// Step 04: Define what happens when the response comes back (onload)
+request.onload = function() {
+    var result = JSON.parse(request.response);
+    for (var i in result) {
+    console.log(result[i].name);
+        console.log(result[i].region);
+        console.log(result[i].subregion);
+        console.log(result[i].population);
+    }
+}
